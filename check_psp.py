28e3fd360fdcf3679524d77c0eb903afe5111f0a
Asadafridi84 <gm.for.bu@gmail.com>
2026-04-14 20:57:45 +0200

    fix: add 78px product image back to PSP cards

---
+++ check_psp.py
@@ -0,0 +1,10 @@
+﻿import re
+c = open('src/components/ProductSearchPanel.jsx', encoding='utf-8').read()
+
+# Add image back before the card body - find the drag label and insert img before it
+old = '<img src={p.img} alt={p.name} style={{ width: "100%", height: "78px", objectFit: "cover", display: "block", pointerEvents: "none" }} />'
+print('img tag present:', old in c)
+
+# Check what the card currently looks like
+idx = c.find('psp-drag')
+print(c[max(0,idx-300):idx+100])
---
+++ fix_psp9.py
@@ -0,0 +1,13 @@
+﻿c = open('src/components/ProductSearchPanel.jsx', encoding='utf-8').read()
+
+old = '                <span className="drag-badge">DRAG</span>\n                <div style={{ padding: '
+new = '                <span className="drag-badge">DRAG</span>\n                <img src={p.img} alt={p.name} style={{ width: "100%", height: "78px", objectFit: "cover", display: "block", pointerEvents: "none" }} />\n                <div style={{ padding: '
+
+c2 = c.replace(old, new)
+if c2 != c:
+    open('src/components/ProductSearchPanel.jsx', 'w', encoding='utf-8').write(c2)
+    print('Fixed!')
+else:
+    print('NOT FOUND - showing context:')
+    idx = c.find('drag-badge')
+    print(repr(c[idx:idx+200]))
